6def66ade7784c7e4ecb6470b09aabe665980623
shivansh84ya <shivanshchourasia84.com>
2022-10-10 22:57:20 +0530

    Code Updated

---
+++ problem.js
@@ -0,0 +1,11 @@
+let name = "Shovansh Chourasia"
+
+console.log(name)
+
+let father_name = "Suresh Chourasia"
+
+console.log(father_name)
+
+let mother_name = "Arti Chourasia"
+
+console.log(mother_name)
